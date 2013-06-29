8ea0be613bfce72e44ad60fddd58785cb4b83bcf
Lloyd Brookes <lloyd.brookes@gmail.com>
2013-06-29 21:14:40 +0100

    basics

--- cli.js
+++ cli.js
@@ -1,29 +1,17 @@
 #!/usr/bin/env node
 "use strict";
 
-var y2md = require("./lib/yuidoc2md");
+var y2md = require("./lib/yuidoc2md"),
+    fs = require("fs"),
+    path = require("path"),
+    Thing = require("nature").Thing;
 
 global.l = console.log;
 process.argv.splice(0, 2);
 
-var markdowns = y2md.getMarkdown(process.argv);
-l(markdowns);
+var options  = new Thing()
+    .mixIn(y2md.getMarkdown.getOptions())
+    .set(process.argv);
 
-// var paths = {
-//         templates: path.resolve("doc-generation", "templates"),
-//         output: path.resolve("doc-generation", "md"),
-//         classTemplate: path.resolve("doc-generation", "templates", "class.mustache"),
-//         debug: path.resolve("doc-generation", "debug")
-//     },
-// 
-//     if (debug){
-//         grunt.file.write(
-//             path.resolve(paths.debug, className + ".json"), 
-//             JSON.stringify(yuidocData, null, "\t")
-//         );
-//     }
-//     
-// mdPath = path.resolve(paths.output, className + ".md");
-// 
-// grunt.file.write(mdPath, output);
-// grunt.log.ok(mdPath + " created.");
+var markdowns = y2md.getMarkdown(options);
+l(markdowns);
