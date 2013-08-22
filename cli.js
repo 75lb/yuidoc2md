5bd79bc82e3ba65343adba20c93f78fab7051fb8
Lloyd Brookes <lloyd.brookes@gmail.com>
2013-08-22 12:26:14 +0200

    error and invalid handling

--- cli.js
+++ cli.js
@@ -6,20 +6,40 @@ var y2md = require("./lib/yuidoc2md"),
     path = require("path"),
     Thing = require("nature").Thing;
 
-var options  = new Thing()
-    .mixIn(new y2md.getMarkdownOptions(), "getMD")
-    .define({ name: "output-dir", alias: "o", default: "yuidoc2md" })
-    .set(process.argv);
-
-var generatedDocs = y2md.getMarkdown(options.where({ group: "getMD" }));
-
-if (!fs.existsSync(options["output-dir"])){
-    fs.mkdirSync(options["output-dir"]);
+function red(txt){
+    return "\x1b[31m" + txt + "\x1b[0m";
 }
 
-generatedDocs.forEach(function(generatedDoc){
-    fs.writeFileSync(
-        path.resolve(options["output-dir"], generatedDoc.name) + ".md", 
-        generatedDoc.markdown
-    );
-});
+var optionSet  = new Thing()
+    .mixIn(new y2md.getMarkdownOptions(), "getMD")
+    .define({ name: "output-dir", alias: "o", default: "yuidoc2md" })
+    .on("error", function(err){
+        console.error(red("Error: ") + err.message);
+        process.exit(1);
+    })
+    .set(process.argv);
+
+if (optionSet.valid){
+    var generatedDocs = y2md.getMarkdown(optionSet.where({ group: "getMD" }));
+
+    if (!fs.existsSync(optionSet["output-dir"])){
+        fs.mkdirSync(optionSet["output-dir"]);
+    }
+
+    if (generatedDocs){
+        generatedDocs.forEach(function(generatedDoc){
+            fs.writeFileSync(
+                path.resolve(optionSet["output-dir"], generatedDoc.name) + ".md", 
+                generatedDoc.markdown
+            );
+        });
+    }
+
+} else {
+    console.error(red("Some option values were invalid"));
+    optionSet.validationMessages.forEach(function(prop){
+        prop.validationMessages.forEach(function(msg){
+            console.error(prop.property, ":\t", msg);
+        });
+    });
+}
