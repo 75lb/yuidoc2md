c26882428bfedc1b59153aef29acd270f35de394
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-02-21 20:23:25 +0000

    fixes #1

--- test/fixture/ComboBox.js
+++ test/fixture/ComboBox.js
@@ -33,8 +33,10 @@ function ComboBox(arg1, arg2){
 ComboBox.prototype.methodA = function(){};
 
 /**
-@method methodA
+@method methodB
+@param {Object} options An options hash
+    @param {String} options.one First thing
+    @param {Number} options.two Second thing
 @return {Boolean} return val
 */
 ComboBox.prototype.methodB = function(){};
-
