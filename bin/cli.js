7b5b4555c8c73a457f30986de2f0de84f60c1b97
Lloyd Brookes <lloyd.brookes@gmail.com>
2014-06-04 11:10:29 +0100

    switched to command-line-args

--- bin/cli.js
+++ bin/cli.js
@@ -3,7 +3,7 @@
 
 var y2md = require("../lib/yuidoc2md"),
     mfs = require("more-fs"),
-    Model = require("nature").Model,
+    cliArgs = require("command-line-args"),
     dope = require("console-dope");
 
 var usage = "Usage:\n\
@@ -13,16 +13,17 @@ $ yuidoc2md [options] <files>\n\
 -j, --json        Output json\n\
 -h, --help        Print this help\n";
 
-var argv  = new Model()
-    .define({ name: "help", type: "boolean", alias: "h" })
-    .define({ name: "input", alias: "i", type: Array, defaultOption: true })
-    .define({ name: "template", alias: "t", type: "string" })
-    .define({ name: "json", type: "boolean", alias: "j" })
-    .on("error", function(err){
-        dope.red.error("Error: " + err.message);
-        process.exit(1);
-    })
-    .set(process.argv);
+try{
+	var argv  = cliArgs([
+		{ name: "help", type: Boolean, alias: "h" },
+		{ name: "input", alias: "i", type: Array, defaultOption: true },
+		{ name: "template", alias: "t", type: String },
+		{ name: "json", type: Boolean, alias: "j" }
+	]).parse();
+} catch (err){
+    dope.red.error("Error: " + err.message);
+    process.exit(1);
+}
 
 /* End here with usage instructions if `--help` or no `--input` specified */
 if (argv.help || (!argv.input)){
@@ -30,27 +31,21 @@ if (argv.help || (!argv.input)){
     process.exit(0);
 }
 
-if (argv.valid){
-    var stats = new mfs.FileSet(argv.input);
-    var result = argv.json
-        ? JSON.stringify(y2md.getJson(stats.files), null, "   ")
-        : y2md.getMarkdown(stats.files, argv.template);
-    process.stdout.write(result);
-    
-    if (stats.files.length){
-        dope.green.underline.error("Processed:");
-        dope.error(stats.files.join("\n").trim());
-    }
-    if (stats.dirs.length){
-        dope.cyan.underline.error("Not files, ignored:");
-        dope.error(stats.dirs.join("\n").trim());
-    }
-    if (stats.notExisting.length){
-        dope.cyan.underline.error("Do not exist:");
-        dope.error(stats.notExisting.join("\n").trim());
-    }
+var stats = new mfs.FileSet(argv.input);
+var result = argv.json
+    ? JSON.stringify(y2md.getJson(stats.files), null, "   ")
+    : y2md.getMarkdown(stats.files, argv.template);
+process.stdout.write(result);
 
-} else {
-    dope.red.error("Some option values were invalid");
-    dope.error(argv.validationMessages.toString());
+if (stats.files.length){
+    dope.green.underline.error("Processed:");
+    dope.error(stats.files.join("\n").trim());
+}
+if (stats.dirs.length){
+    dope.cyan.underline.error("Not files, ignored:");
+    dope.error(stats.dirs.join("\n").trim());
+}
+if (stats.notExisting.length){
+    dope.cyan.underline.error("Do not exist:");
+    dope.error(stats.notExisting.join("\n").trim());
 }
